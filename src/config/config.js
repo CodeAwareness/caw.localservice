d3e1a014c0bce0bc9df2094e48280b9a0dc1e8cc
Mark Vasile <marian.vasile@gmail.com>
2021-05-29 01:20:16 +0900

    split api for now

--- src/config/config.js
+++ src/config/config.js
@@ -33,7 +33,7 @@ if (error) {
 }
 
 const PORT_LOCAL = envVars.PORT || 48048
-const API_URL = process.env.LOCAL ? `http://localhost:${PORT_LOCAL}/api/v1` : 'https://api.peer8.com/v1'
+const API_URL = process.env.LOCAL ? `http://localhost:${PORT_LOCAL}/api/v1` : 'https://ppt.peer8.com/v1'
 
 const CONFIGURATION_FILE = '.peer8'
 const PEER8_SCHEMA = 'peer8'
