7b02e298c2e4a7a09804739e176e1e80b09e6356
Mark Vasile <marian.vasile@gmail.com>
2021-12-26 19:54:07 +0900

    got the jest module resolution working

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+const { pathsToModuleNameMapper } = require('ts-jest')
+
 module.exports = {
+  moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleDirectories: ['node_modules', 'src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
 };
