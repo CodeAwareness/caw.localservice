53e8acf295883947669f3f1da3336c12e6f0a4c4
Atul Goel <atul.goel.126+github@gmail.com>
2022-08-16 19:17:03 -0700

    Added comments to the route files.

--- src/routes/v1/gardener.user.route.ts
+++ src/routes/v1/gardener.user.route.ts
@@ -2,6 +2,8 @@ import type { CΩExpress } from '@/app'
 import app from '@/app'
 import userController from '@/controllers/user.controller'
 
+// Sends request to the API on the cloud.
+// Uses both Sockets and HTTP.
 const router = {
   init: (): void => {
     const socket = (app as CΩExpress).gardenerSocket
--- src/routes/v1/gstation.auth.route.ts
+++ src/routes/v1/gstation.auth.route.ts
@@ -1,6 +1,9 @@
 import type { Socket } from 'socket.io'
 import authController from '@/controllers/auth.controller'
 
+// this is where all applications on the OS will submit request to.
+// All are socket requests.
+
 const router = {
   init: (socket: Socket): void => {
     socket.on('auth:info', authController.info)
