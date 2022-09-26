73b0c5debb427e13e6d1c05414e01ab595a19f81
CodeAwareness <codeawareness@gmail.com>
2022-09-26 16:13:56 -0700

    better logging

--- src/logger.ts
+++ src/logger.ts
@@ -12,35 +12,53 @@ const circularReplacer = () => {
   }
 }
 
+function getStackTrace() {
+  const obj: any = {}
+  Error.captureStackTrace(obj, getStackTrace)
+  return obj.stack
+}
+
 function stringify(arg) {
   return JSON.stringify(arg, circularReplacer())?.substr(0, 1500)
+}
+
+function trace() {
+  const lines = getStackTrace().split('\n').filter(l => (l.includes('localservice') && !l.includes('logger') && !l.includes('node_modules')))
+  console.debug(lines.join('\n'))
 }
 
 /* eslint-disable no-console */
 /* eslint-disable n/handle-callback-err */
 const logger = {
   log: function(...args: any[]): void {
+    console.group()
     console.log('CΩ.LS:', map(args, stringify))
+    trace()
     args.unshift('GRAND STATION')
     api.axiosAPI.post(`${api.API_LOG}/log`, args).catch(err => {})
+    console.groupEnd()
   },
   info: function(...args: any[]): void {
     console.info('CΩ.LS:', map(args, stringify))
+    trace()
     args.unshift('GRAND STATION')
     api.axiosAPI.post(`${api.API_LOG}/info`, args).catch(err => {})
   },
   warn: function(...args: any[]): void {
     console.warn('CΩ.LS:', map(args, stringify))
+    trace()
     args.unshift('GRAND STATION')
     api.axiosAPI.post(`${api.API_LOG}/warn`, args).catch(err => {})
   },
   debug: function(...args: any[]): void {
     console.info('CΩ.LS:', map(args, stringify))
+    trace()
     args.unshift('GRAND STATION')
     api.axiosAPI.post(`${api.API_LOG}/debug`, args).catch(err => {})
   },
   error: function(...args: any[]): void {
     console.error('CΩ.LS:', map(args, stringify))
+    trace()
     args.unshift('GRAND STATION')
     api.axiosAPI.post(`${api.API_LOG}/error`, args).catch(err => {})
   },
